dadb46084ee3e71b84e6c03333af655548a02af9
Woy-tek <woy1157@gmail.com>
2018-11-21 20:56:34 +0100

    Adds input

---
+++ app/src/main/java/com/example/agh/retrofitapp/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.agh.retrofitapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText text = (EditText) findViewById(R.id.editText);
+                String name = text.getText().toString();
+                getInfo(name);
+            }
+        });
+
+    }
+
+    public void getInfo(String name){
+
+        listView = (ListView) findViewById(R.id.pagination_list);
+
+        Retrofit.Builder builder = new Retrofit.Builder()
+                .baseUrl("https://api.github.com/")
+                .addConverterFactory(GsonConverterFactory.create());
+
+        Retrofit retrofit = builder.build();
+
+        GitHubClient client = retrofit.create(GitHubClient.class);
+        Call<List<GitHubRepo>> call = client.reposForUser(name);
+
+        call.enqueue(new Callback<List<GitHubRepo>>() {
+            @Override
+            public void onResponse(Call<List<GitHubRepo>> call, Response<List<GitHubRepo>> response) {
+                List<GitHubRepo> repos = response.body();
+                listView.setAdapter(new GitHubRepoAdapter(MainActivity.this, repos));
+            }
+
+            @Override
+            public void onFailure(Call<List<GitHubRepo>> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "ERROR " + t.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+}
